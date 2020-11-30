63c97ddd50586e5ae651a59e0ec0aaa11c93e5ca
Fred Hoogduin <dev@hoogduin.net>
2020-11-30 21:38:11 +0100

    empty framework added

---
+++ src/AdventConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode
+{
+    public static class AdventConfig
+    {
+        public const int DefaultYear = 2020;
+        public const int DefaultDay = 1;
+        public const string LogFileFolder = @"C:\Users\fred\Documents\GitHub\AdventOfCode2020\logs";
+        public const string InputFileFolder = @"C:\Users\fred\Documents\GitHub\AdventOfCode2020\input";
+        public const string TestFileFolder = @"C:\Users\fred\Documents\GitHub\AdventOfCode2020\tests";
+        public const string SessionCookieFile = @"C:\AoC\AoCSessionCookie.txt";
+        public const bool LogFilesEnabled = true;
+    }
+}
