13293c680d947ed9444f644441e7e1f392abd74b
Fred Hoogduin <dev@hoogduin.net>
2020-12-03 09:22:15 +0100

    day 3 solved

--- src/Days/Day03.cs
+++ src/Days/Day03.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 
 namespace AdventOfCode.Days
 {
@@ -9,12 +7,40 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            // read input into grid, and traverse it
+            var map = input.CreateCharGrid();
+            return TreeCount(map, (3, 1)).ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            // read input into grid
+            var map = input.CreateCharGrid();
+            // traverse grid for all slopes, multiplying the results
+            var result =
+                new[] {(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)}.Aggregate(1L,
+                    (current, slope) => current * TreeCount(map, slope));
+            return result.ToString();
+        }
+
+        private static int TreeCount(char[,] map, (int right, int down) valueTuple)
+        {
+            // walk the map, simulating infinite right by wrapping x 
+            var (right, down) = valueTuple;
+            var lowerBoundary = map.GetUpperBound(1);
+            var rightBoundary = map.GetUpperBound(0);
+            var x = 0;
+            var y = 0;
+            var trees = 0;
+
+            while (y < lowerBoundary)
+            {
+                y += down;
+                x = (x + right) % (rightBoundary + 1);
+                if (map[x, y] == '#') trees++;
+            }
+
+            return trees;
         }
     }
 }
