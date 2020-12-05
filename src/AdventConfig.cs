2354f9b1c48e3e5942c50e758ae959f0c6a2cd86
Fred Hoogduin <dev@hoogduin.net>
2020-12-05 12:14:03 +0100

    day 5 solved

--- src/AdventConfig.cs
+++ src/AdventConfig.cs
@@ -3,7 +3,7 @@
     public static class AdventConfig
     {
         public const int DefaultYear = 2020;
-        public const int DefaultDay = 4;
+        public const int DefaultDay = 5;
         public const string LogFileFolder = @"C:\Users\fred\Documents\GitHub\AdventOfCode2020\logs";
         public const string InputFileFolder = @"C:\Users\fred\Documents\GitHub\AdventOfCode2020\input";
         public const string TestFileFolder = @"C:\Users\fred\Documents\GitHub\AdventOfCode2020\tests";
--- src/Days/Day05.cs
+++ src/Days/Day05.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Days
@@ -7,14 +6,27 @@ namespace AdventOfCode.Days
     [Day(2020, 5)]
     public class Day05 : BaseDay
     {
+        // lol, 7 characters for 128 possibilities doesn't that remind
+        // us of something? :) And again 3 chars for 8 possibilities.
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var passes = input.Lines();
+            return passes.Max(SeatId).ToString();
         }
 
+        // Linq FTW
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var orderedPasses = input.ParseStrings(SeatId).OrderBy(x => x).ToArray();
+            return Enumerable.Range(orderedPasses[0], orderedPasses[^1] - orderedPasses[0] + 1)
+                .Except(orderedPasses).First().ToString();
+        }
+
+        private static int SeatId(string arg)
+        {
+            // swap letters to binary digits and return result
+            var binaryString = arg.Replace('B', '1').Replace('F', '0').Replace('R', '1').Replace('L', '0');
+            return Convert.ToInt32(binaryString, 2);
         }
     }
 }
