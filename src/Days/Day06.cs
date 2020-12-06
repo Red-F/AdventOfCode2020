6c15ce086c78e0be37316fe78471943a5333f64f
Fred Hoogduin <dev@hoogduin.net>
2020-12-06 13:47:49 +0100

    day 6 solved

--- src/Days/Day06.cs
+++ src/Days/Day06.cs
@@ -1,20 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Days
 {
+    /// <summary>
+    /// Who said C# was more readable than F#? ;-)
+    /// Heavy Linq but still performant
+    /// </summary>
     [Day(2020, 6)]
     public class Day06 : BaseDay
     {
-        public override string PartOne(string input)
-        {
-            throw new NotImplementedException();
-        }
+        public override string PartOne(string input) =>
+            input.LineGroups(Environment.NewLine + Environment.NewLine).Sum(CountGroup1).ToString();
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            return input.LineGroups(Environment.NewLine + Environment.NewLine)
+                .Sum(s => CountGroup2(s.Where(char.IsLetter).ToArray(), s.Lines().Count())).ToString();
         }
+
+
+        private static int CountGroup1(string arg) => arg.Where(char.IsLetter).Distinct().Count();
+
+        private static int CountGroup2(char[] arg, int count) =>
+            arg.Distinct().Count(c => arg.Count(x => x == c) == count);
     }
 }
