0f811b042d5cad01e4ebd293175e3584ceb392e8
Fred Hoogduin <dev@hoogduin.net>
2020-12-15 11:06:59 +0100

    day 15 solved

--- src/Days/Day15.cs
+++ src/Days/Day15.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode.Days
@@ -9,12 +8,40 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var startingNumbers = input.Longs().ToArray();
+            var memory = new long[2020];
+            Array.Copy(startingNumbers, memory, startingNumbers.Length);
+            for (var i = startingNumbers.Length; i < 2020; i++)
+            for (var j = i - 2; j >= 0; j--)
+            {
+                if (memory[j] != memory[i - 1]) continue;
+                memory[i] = i - j - 1;
+                break;
+            }
+            return memory[2019].ToString();
         }
 
         public override string PartTwo(string input)
         {
-            throw new NotImplementedException();
+            var startingNumbers = input.Longs().ToArray();
+            var memory = new long[30000000];
+            for (var i = 0; i < startingNumbers.Length - 1; i++) memory[startingNumbers[i]] = i + 1;
+            var nextNumber = startingNumbers[^1];
+            for (var i = startingNumbers.Length + 1; i <= 30000000; i++)
+            {
+                if (memory[nextNumber] == 0)
+                {
+                    memory[nextNumber] = i - 1;
+                    nextNumber = 0;
+                }
+                else
+                {
+                    var newNumber = i - 1 - memory[nextNumber];
+                    memory[nextNumber] = i - 1;
+                    nextNumber = newNumber;
+                }
+            }
+            return nextNumber.ToString();
         }
     }
 }
